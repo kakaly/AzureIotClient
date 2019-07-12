6be3a069d4c5eb7e47e4296af7bde9e33d9d6d04
kakaly <reach.kkarthik@gmail.com>
2019-07-12 10:39:38 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <body>
+    <div class="container">
+    <div class="row">
+    <nav>
+    <div class="nav-wrapper teal lighten-2">
+      <div class="brand-logo center">Pharos TrustCenter Management</div>
     </div>
+    </nav>
+    <table class="highlight">
+    <thead>
+      <tr class="grey lighten-2">
+          <th>Device</th>
+          <th>Status</th>
+          <th>FQDN</th>
+          <th>Command</th>
+          <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>PCOIP_DEVICE_01</td>
+        <td>InSession</td>
+        <td>10.0.1.2</td>
+        <div class="input-field col s6">
+          <input type="text" id="autocomplete-input1" class="autocomplete"/>
+          <label for="autocomplete-input1">Command</label>
+        </div>
+        <td>
+          <button class="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td>PCOIP_DEVICE_02</td>
+        <td>InSession</td>
+        <td>10.0.1.4</td>
+        <div class="input-field col s6">
+          <input type="text" id="autocomplete-input2" class="autocomplete"/>
+          <label for="autocomplete-input2">Command</label>
+        </div>
+        <td>
+          <button class="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </td>
+      </tr>
+      <tr>
+        <td>PCOIP_DEVICE_03</td>
+        <td>PreSession</td>
+        <td>10.0.1.3</td>
+        <div class="input-field col s6">
+          <input type="text" id="autocomplete-input3" class="autocomplete"/>
+          <label for="autocomplete-input3">Command</label>
+        </div>
+        <td>
+          <button class="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
+            <i class="material-icons right">send</i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+  </div>
+  </body>
   );
 }
 
