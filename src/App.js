f6f7f4dafeafc2422db0a9168083df8da904c3c3
kakaly <reach.kkarthik@gmail.com>
2019-07-14 17:12:39 -0700

    Final app

--- src/App.js
+++ src/App.js
@@ -1,104 +1,142 @@
 import React, {Component} from 'react';
-import iothub from 'azure-iothub';
 import './App.css';
-
-
 class App extends Component {
   constructor(props) {
     super(props)
-    this.connectionString = ''
-    this.registry = ''
+    this.state = {
+      devices: [],
+      twins: [],
+      deviceState: {},
+      data: [],
+      loading: 'initial'
+    }
   }
 
-  componentWillMount() {
-    this.connectionString = 'HostName=PharosTrustCenterHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=yfeQ47idW0OODIhf0ewIGJ/gPTfYYyVRohR1hnMfCi4=';
-    this.registry = iothub.Registry.fromConnectionString(this.connectionString);
-
+  componentDidMount() {
+    var api = 'http://localhost:4000/getdevices'
+    this.setState({
+      loading: true
+    })
+    var twins = this.state.twins
+    fetch(api, {
+        method: 'GET',
+      }).then(function(response) {
+        return response.json()
+      }).then(function(data) {
+        var devices = JSON.parse(JSON.stringify(data))
+        devices.forEach(device => {
+          this.getDeviceTwin(device.deviceId, twins)
+        })
+        var deviceState = {}
+        devices.forEach(device => deviceState[device.deviceId] = 'off')
+        this.setState({
+          devices: devices,
+          deviceState: deviceState,
+          loading: false
+        })
+      }.bind(this)).catch(error => {
+        console.log(error)
+      })
   }
 
-  queryTwin = async () => {
-    this.registry.getTwin('pcoip_device01', function(err, twin){
-        if (err) {
-            console.error(err.constructor.name + ': ' + err.message);
-        } else {
-            console.log(twin.properties)
-            return [twin.deviceId,
-                    twin.properties.reported.insession,
-                    twin.properties.reported.fqdn]
-        }
-    });
-  };
+  async getDeviceTwin(deviceId, twins) {
+    var api = 'http://localhost:4000/gettwin/' + deviceId
+    this.setState({
+      loading: true
+    })
+    fetch(api, {
+        method: 'GET',
+      }).then((response) => response.json())
+      .then(function(data) {
+        var deviceTwin = JSON.parse(JSON.stringify(data))
+        twins.push(deviceTwin)
+        this.setState({
+          twins: twins,
+          loading: false
+        })
+      }.bind(this)).catch(error => {
+        console.log(error)
+      })
+  }
+
+  async getDeviceData(deviceId, data) {
+    var api = 'http://localhost:4000/getdata/'
+    this.setState({
+      loading: true
+    })
+    fetch(api, {
+        method: 'GET',
+      }).then((response) => response.json())
+      .then(function(data) {
+        var deviceData = JSON.parse(JSON.stringify(data))
+        data.push(deviceData)
+        this.setState({
+          data: data,
+          loading: false
+        })
+      }.bind(this)).catch(error => {
+        console.log(error)
+      })
+  }
+
+  async enableDevice(deviceId) {
+    var enable = this.state.deviceState[deviceId]
+    enable = (enable === 'off')?'on':'off'
+    var deviceState = this.state.deviceState
+    deviceState[deviceId] = enable
+    this.setState({deviceState: deviceState})
+    var api = 'http://localhost:4000/updatestate/' + deviceId +  '/' + enable
+    console.log(api)
+    fetch(api, {
+        method: 'GET',
+      }).then((response) => {
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+  }
 
   render() {
-    var device = this.queryTwin()
-    console.log(device)
+    var twins = this.state.twins
+    console.log(twins)
     return (
-      <body>
-      <div className="container">
-      <div className="row">
-      <nav>
-      <div className="nav-wrapper teal lighten-2">
-        <div className="brand-logo center">Pharos TrustCenter Management</div>
-      </div>
-      </nav>
-      <table className="highlight">
-      <thead>
-        <tr className="grey lighten-2">
-            <th>Device</th>
-            <th>Status</th>
-            <th>FQDN</th>
-            <th>Command</th>
-            <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>PCOIP_DEVICE_01</td>
-          <td>InSession</td>
-          <td>10.0.1.2</td>
-          <div className="input-field col s6">
-            <input type="text" id="autocomplete-input1" className="autocomplete"/>
-            <label for="autocomplete-input1">Command</label>
+      this.state.loading === false ? (
+          <div className="container">
+          <div className="row">
+          <nav>
+          <div className="nav-wrapper orange lighten-2">
+            <div className="brand-logo center">Street View Devices Control Center</div>
           </div>
-          <td>
-            <button className="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
-              <i className="material-icons right">send</i>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>PCOIP_DEVICE_02</td>
-          <td>InSession</td>
-          <td>10.0.1.4</td>
-          <div className="input-field col s6">
-            <input type="text" id="autocomplete-input2" className="autocomplete"/>
-            <label for="autocomplete-input2">Command</label>
+          </nav>
+          <table className="highlight">
+          <thead>
+            <tr className="grey lighten-2">
+                <th>Device</th>
+                <th>Connection Status</th>
+                <th>Status Update Time</th>
+                <th>Enable Device</th>
+            </tr>
+          </thead>
+          <tbody>
+            {twins.map((twin,i) => {
+              return (
+                <tr key={i}>
+                  <td>{twin.deviceId}</td>
+                  <td>{twin.connectionState}</td>
+                  <td>{twin.statusUpdateTime}</td>
+                  <td className="switch">
+                    <label>
+                      Off
+                      <input type="checkbox" onChange={() => this.enableDevice(twin.deviceId)}/>
+                      <span className="lever"></span>
+                      On
+                    </label>
+                  </td>
+                </tr>)})}
+          </tbody>
+          </table>
           </div>
-          <td>
-            <button className="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
-              <i className="material-icons right">send</i>
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>PCOIP_DEVICE_03</td>
-          <td>PreSession</td>
-          <td>10.0.1.3</td>
-          <div className="input-field col s6">
-            <input type="text" id="autocomplete-input3" className="autocomplete"/>
-            <label for="autocomplete-input3">Command</label>
-          </div>
-          <td>
-            <button className="btn waves-effect waves-light teal lighten-2" type="submit" name="action">Submit
-              <i className="material-icons right">send</i>
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    </div>
-    </div>
-    </body>
+          </div>):<div>Loading...</div> 
     );
   }
 }
